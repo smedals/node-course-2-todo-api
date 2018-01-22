b85bac2913c57469ca52875ddc9ab28b41a9dbb0
Medals <steven.medals@hpe.com>
2018-01-22 11:27:45 -0600

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: 'First test todo'
+}, {
+	text: 'Second test todo'
+}];
+
 beforeEach((done) => {
 	Todo.remove({}).then(() => {
-		done();
-	})
+		return Todo.insertMany(todos);
+	}).then(() => {
+		done()
+	});
 });
 
 describe('POST /todos', () => {
@@ -25,13 +33,27 @@ describe('POST /todos', () => {
 				if (err) {
 					return done(err);
 				}
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
 				}).catch((err) => {
-					done(e);
+					done(err);
 				})
 			})
+	});
+
+
+});
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	})
 })
