797af96bbb2e75fb759fa69ac4081512b7804808
Medals <steven.medals@hpe.com>
2018-01-22 13:34:03 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -26,6 +27,23 @@ app.get('/todos', (req,res) => {
 		res.send({todos});
 	},(e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req,res) => {
+	var todoId = req.params.id;
+	if (!ObjectID.isValid(todoId)) {
+		console.log('id not valid');
+		return res.status(404).send();
+	}
+	Todo.findById(todoId).then((todo) => {
+		if (todo) {
+			res.send({todo}); 
+		} else {
+			res.status(404).send();
+		}
+	}, (err) => {
+		res.status(400).send();
 	});
 });
 
