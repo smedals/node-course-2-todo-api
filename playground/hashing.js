2f0745a5576360af461eb4a2625719a28e1c8326
Medals <steven.medals@hpe.com>
2018-01-23 14:22:37 -0600

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10,(err,salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+var hashedPassword = '$2a$10$.TkY1gGiBkUnP.RfvjY0C.2WB8KiZZEZr6DKj.Stpzz2w7Gj76/j6'
+
+bcrypt.compare(password, hashedPassword, (err, result) => {
+	console.log(result);
+})
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
 
-var decoded =jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+// var decoded =jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
