1b802992e226238134e94f57f2122eebee159cd6
Medals <steven.medals@hpe.com>
2018-01-24 15:25:54 -0600

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -209,7 +209,7 @@ describe('POST /users', () => {
 				User.findOne({email}).then((user) => {
 					expect(user).toExist;
 					done();
-				})
+				}).catch((e) => done(e));
 			});
 	});
 
@@ -235,3 +235,50 @@ describe('POST /users', () => {
 			.end(done);
 	});
 });
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toExist;
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens[0].access).toBe('auth');
+					expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should reject invalid login', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password + '1'
+			})
+			.expect(400)
+			.expect((res) => {
+				expect(res.headers['x-auth']).toNotExist;
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+})
