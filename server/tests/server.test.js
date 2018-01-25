c5069fba84e2e3b73f1755068f293b8f715d98b5
Medals <steven.medals@hpe.com>
2018-01-25 10:48:54 -0600

    update to expect library

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -119,7 +119,7 @@ describe('DELETE /todos/:id', () => {
 				}
 
 				Todo.findById(hexId).then((todo) => {
-					expect(todo).toNotExist;
+					expect(todo).toBeFalsy();
 					done();
 				}).catch((e) => done(e));
 			});
@@ -138,7 +138,7 @@ describe('DELETE /todos/:id', () => {
 				}
 
 				Todo.findById(hexId).then((todo) => {
-					expect(todo).toExist;
+					expect(todo).toBeTruthy();
 					done();
 				}).catch((e) => done(e));
 			});
@@ -205,7 +205,7 @@ describe('PATCH /todos/:id', () => {
 			.expect((res) => {
 				expect(res.body.todo.text).toBe(text);
 				expect(res.body.todo.completed).toBe(false);
-				expect(res.body.todo.completedAt).toNotExist;
+				expect(res.body.todo.completedAt).toBeFalsy();
 			})
 			.end(done);
 	})
@@ -245,8 +245,8 @@ describe('POST /users', () => {
 			.send({email, password})
 			.expect(200)
 			.expect((res) => {
-				expect(res.headers['x-auth']).toExist;
-				expect(res.body._id).toExist;
+				expect(res.headers['x-auth']).toBeTruthy();
+				expect(res.body._id).toBeTruthy();
 				expect(res.body.email).toBe(email);
 			})
 			.end((err) => {
@@ -254,7 +254,8 @@ describe('POST /users', () => {
 					return done(err);
 				}
 				User.findOne({email}).then((user) => {
-					expect(user).toExist;
+					expect(user).toBeTruthy();
+					expect(user.password).not.toBe(password);
 					done();
 				}).catch((e) => done(e));
 			});
@@ -293,7 +294,7 @@ describe('POST /users/login', () => {
 			})
 			.expect(200)
 			.expect((res) => {
-				expect(res.headers['x-auth']).toExist;
+				expect(res.headers['x-auth']).toBeTruthy();
 			})
 			.end((err, res) => {
 				if (err) {
@@ -316,7 +317,7 @@ describe('POST /users/login', () => {
 			})
 			.expect(400)
 			.expect((res) => {
-				expect(res.headers['x-auth']).toNotExist;
+				expect(res.headers['x-auth']).toBeFalsy();
 			})
 			.end((err, res) => {
 				if (err) {
